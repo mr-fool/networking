1dd9153914a3d23086dcce665fe49a5f6b45353c
mr-fool <mr-fool@users.noreply.github.com>
2017-09-18 11:41:36 -0600

    Update server.py

--- python/server.py
+++ python/server.py
@@ -4,3 +4,14 @@ server_socket = socket.socket()
 
 
 server_socket.blind( ('localhost' , 8888 ) )
+server_socket.listen(0)
+
+try:
+	while True:
+		print ("Waiting for a client")
+		
+		client_socket, info = server_socket.accept()
+
+except Exception as err:
+	print("An Exception has occured: {}".format(err) )
+    
