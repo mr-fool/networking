19b33c4dd4654e497c0482173d3306d498189f73
mr-fool <mr-fool@users.noreply.github.com>
2017-09-18 11:44:45 -0600

    Update server.py

--- python/server.py
+++ python/server.py
@@ -3,7 +3,7 @@ import socket
 server_socket = socket.socket()
 
 
-server_socket.blind( ('localhost' , 8888 ) )
+server_socket.bind( ('localhost' , 8888 ) )
 server_socket.listen(0)
 
 try:
@@ -14,6 +14,6 @@ try:
 
 except Exception as err:
 	print("An Exception has occured: {}".format(err) )
-    pass
+	pass
     
 server_socket.close()
